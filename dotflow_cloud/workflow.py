85083ac182ea08604447cd513b91ac56dd76d639
Fernando Celmer <email@fernandocelmer.com>
2026-04-13 23:56:19 -0300

    ⚙️ FEATURE: Wire workflow with endpoints pipeline and initial context

--- dotflow_cloud/workflow.py
+++ dotflow_cloud/workflow.py
@@ -1,11 +1,30 @@
 from dotflow import DotFlow
 
-from dotflow_cloud.actions import step_one, step_two
+from dotflow_cloud.actions import (
+    fetch_all_endpoints,
+    aggregate_results,
+    generate_report,
+)
+
+
+ENDPOINTS = [
+    "https://jsonplaceholder.typicode.com/posts/1",
+    "https://jsonplaceholder.typicode.com/posts/2",
+    "https://jsonplaceholder.typicode.com/posts/3",
+    "https://jsonplaceholder.typicode.com/users/1",
+    "https://jsonplaceholder.typicode.com/users/2",
+]
 
 
 def main() -> DotFlow:
     workflow = DotFlow()
-    workflow.task.add(step=step_one)
-    workflow.task.add(step=step_two)
+    workflow.task.add(
+        step=[
+            fetch_all_endpoints,
+            aggregate_results,
+            generate_report,
+        ],
+        initial_context=ENDPOINTS,
+    )
 
     return workflow
