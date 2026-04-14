004e4cf148ebc432bd1e3dc2ac4522c114ad08f2
Fernando Celmer <email@fernandocelmer.com>
2026-04-13 21:30:54 -0300

    ⚙️ FEATURE: Simplify main function and remove startup logic

--- dotflow_cloud/workflow.py
+++ dotflow_cloud/workflow.py
@@ -1,22 +1,11 @@
-from dotflow import Config, DotFlow
+from dotflow import DotFlow
 
 from dotflow_cloud.actions import step_one, step_two
 
-config = Config()
 
-
-def main():
-    workflow = DotFlow(config=config)
-
+def main() -> DotFlow:
+    workflow = DotFlow()
     workflow.task.add(step=step_one)
     workflow.task.add(step=step_two)
-    workflow.start(mode="sequential")
-
-    for task in workflow.result_task():
-        print(f"Task {task.task_id}: {task.status}")
 
     return workflow
-
-
-if __name__ == "__main__":
-    main()
